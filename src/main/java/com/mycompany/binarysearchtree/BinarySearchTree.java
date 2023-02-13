a9d0aceb1a05fe6f540a87dba2873dbc864f21bc
nka2001 <89935053+nka2001@users.noreply.github.com>
2023-02-12 21:38:11 -0500

    BST - Initial Commit

---
+++ src/main/java/com/mycompany/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.mycompany.binarysearchtree;
+
+/**
+ *
+ * @author nicka
+ */
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
